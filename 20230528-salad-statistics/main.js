967b38890112649215897c7de54f8278e3d07846
jpearce2014 <jlpearce.phd@gmail.com>
2023-05-28 15:37:35 -0400

    local storage is not easy to clear, but maybe a reset button?

--- 20230528-salad-statistics/main.js
+++ 20230528-salad-statistics/main.js
@@ -1,14 +1,22 @@
-let button = document.getElementById("my-button");
-let clickCount = 0;
-let display = document.getElementById("my-button");
 
-const clickCountFxn = () => {
-    clickCount++;
-    display.innerHTML = `Button clicked: ${clickCount} times.`;
-    localStorage.setItem("clickCount", clickCount);
-}
+// =============
+// Using Local Storage
+// Pro: Persists after refresh or close, so I don't lose data.
+// Con: Cannot easily reset....
+// or can I?
+// =============
 
-if (localStorage.getItem("clickCount")) {
-    clickCount = parseInt(localStorage.getItem("clickCount"));
-}
-button.addEventListener("click", clickCountFxn);
+// let button = document.getElementById("my-button");
+// let clickCount = 0;
+// let display = document.getElementById("click-count");
+
+// const clickCountFxn = () => {
+//     clickCount++;
+//     display.innerHTML = `Button clicked: ${clickCount} times.`;
+//     localStorage.setItem("clickCount", clickCount);
+// }
+
+// if (localStorage.getItem("clickCount")) {
+//     clickCount = parseInt(localStorage.getItem("clickCount"));
+// }
+// button.addEventListener("click", clickCountFxn);
